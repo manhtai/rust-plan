3dd13e2394b5785ae55d8f6d8c797a6d2fbd981a
manhtai <vo@manhtai.com>
2019-10-16 17:42:54 +0700

    Add log, still failing

--- project-2/src/bin/kvs.rs
+++ project-2/src/bin/kvs.rs
@@ -1,8 +1,11 @@
-use clap::{App, Arg, SubCommand};
-use kvs::KvStore;
 use std::process::exit;
+use std::path::Path;
 
-fn main() {
+use clap::{App, Arg, SubCommand};
+
+use kvs::{KvStore, Result};
+
+fn main() -> Result<()> {
     let matches = App::new("kvs")
         .version(env!("CARGO_PKG_VERSION"))
         .author("manhtai")
@@ -24,19 +27,47 @@ fn main() {
         print!(env!("CARGO_PKG_VERSION"))
     }
 
+    let mut store = KvStore::open(Path::new("target/stored.db"))?;
+
     if let Some(matches) = matches.subcommand_matches("set") {
-        eprint!("unimplemented");
-        exit(1)
+        if let Some(key) = matches.value_of("key") {
+            if let Some(value) = matches.value_of("value") {
+                match store.set(key.to_owned(), value.to_owned()) {
+                    Err(err) => println!("{:?}", err),
+                    _ => ()
+                }
+                exit(0)
+            }
+        }
     }
 
     if let Some(matches) = matches.subcommand_matches("get") {
-        eprint!("unimplemented");
-        exit(1)
+        if let Some(key) = matches.value_of("key") {
+            match store.get(key.to_owned()) {
+                Ok(option) => match option {
+                    Some(value) => println!("{}", value),
+                    _ => ()
+                },
+                Err(error) => {
+                    println!("{:?}", error);
+                },
+            };
+            exit(0)
+        }
     }
 
     if let Some(matches) = matches.subcommand_matches("rm") {
-        eprint!("unimplemented");
-        exit(1)
+        if let Some(key) = matches.value_of("key") {
+            let k = key.to_owned();
+            match store.remove(k) {
+                Err(error) =>  {
+                    println!("{:?}", error);
+                    exit(1)
+                },
+                _ => ()
+            };
+            exit(0)
+        }
     }
 
     exit(1)
--- project-2/src/lib.rs
+++ project-2/src/lib.rs
@@ -1,45 +1,122 @@
 use std::collections::HashMap;
+use std::fmt;
+use std::fs::OpenOptions;
+use std::io::{BufRead, BufReader, Write};
 use std::path::Path;
 use std::result;
 
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug)]
 pub enum KvError {
     KeyNotFound,
-    IoError,
+    IoError(String),
+    SerdeError(String),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum Command {
+    Set(String, String),
+    Remove(String),
+}
+
+static FILENAME: &str = "target/store.db";
+
+impl fmt::Debug for KvError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            KvError::KeyNotFound => write!(f, "Key not found"),
+            KvError::IoError(err) => write!(f, "IO error: {}", err),
+            KvError::SerdeError(err) => write!(f, "Serde error: {}", err),
+        }
+    }
 }
 
 pub type Result<T> = result::Result<T, KvError>;
 
 pub struct KvStore {
     storage: HashMap<String, String>,
+    path: String,
 }
 
 impl KvStore {
     pub fn new() -> Self {
         let storage = HashMap::new();
-        KvStore { storage }
+        KvStore { storage, path: FILENAME.to_string() }
     }
 
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
-        self.storage.insert(key, value);
+        self.storage.insert(key.to_owned(), value.to_owned());
+        KvStore::save(&self, &Command::Set(key, value));
         Ok(())
     }
 
     pub fn get(&self, key: String) -> Result<Option<String>> {
-        let result = match self.storage.get(&key) {
-            Some(s) => Some(s.to_owned()),
-            None => None
-        };
-        Ok(result)
-    }
-
-    pub fn open(path: &Path) -> Result<KvStore> {
-        Ok(KvStore::new())
+        match self.storage.get(&key) {
+            Some(s) => Ok(Some(s.to_owned())),
+            None => Err(KvError::KeyNotFound),
+        }
     }
 
     pub fn remove(&mut self, key: String) -> Result<()> {
+        self.get(key.to_owned())?;
         self.storage.remove(&key);
+        KvStore::save(&self, &Command::Remove(key));
         Ok(())
+    }
+
+    pub fn open(path: &Path) -> Result<KvStore> {
+        match OpenOptions::new().read(true).create(true).open(path) {
+            Ok(file) => {
+                let mut store = KvStore::new();
+                store.path = path.to_str().unwrap_or(FILENAME).to_string();
+
+                let reader = BufReader::new(file);
+                for line in reader.lines() {
+                    match line {
+                        Ok(cmd) => match serde_json::from_str::<Command>(&cmd) {
+                            Ok(cmd) => {
+                                KvStore::load(&mut store, &cmd);
+                            }
+                            _ => (),
+                        },
+                        _ => ()
+                    }
+                }
+
+                Ok(store)
+            }
+            Err(err) => Err(KvError::IoError(err.to_string())),
+        }
+    }
+
+    fn load(store: &mut KvStore, command: &Command) -> Result<()> {
+        match command {
+            Command::Set(key, value) => {
+                store.set(key.to_owned(), value.to_owned());
+            }
+            Command::Remove(key) => {
+                store.remove(key.to_owned());
+            }
+        }
+
+        Ok(())
+    }
+
+    fn save(store: &KvStore, command: &Command) -> Result<()> {
+        match OpenOptions::new().append(true).open(&store.path) {
+            Ok(mut file) => {
+                match serde_json::to_string(command) {
+                    Ok(cmd) => {
+                        match writeln!(file, "{}", cmd) {
+                            Ok(_) => Ok(()),
+                            Err(_) => Err(KvError::IoError),
+                        };
+                        Ok(())
+                    }
+                    Err(err) => Err(KvError::IoError(err.to_string())),
+                }
+            }
+            Err(err) => Err(KvError::IoError(err.to_string())),
+        }
     }
 }
